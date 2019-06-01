79a475cada14fbeeaf77ade9c1b58f931b53c0a9
Paweł Bałaga <pbalaga@prescientco.com>
2019-06-01 10:48:17 +0200

    Added: map loader script

---
+++ Assets/Scripts/MapLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MapLoader : MonoBehaviour
+{
+    public string MapName;
+
+    public void LoadMap()
+    {
+        SceneManager.LoadScene(MapName);
+    }
+}
